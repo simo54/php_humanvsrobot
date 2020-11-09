5dce037f571602f152e80d65be13a826015938f4
simone <sorzesimone1@gmail.com>
2020-11-09 16:17:43 +0100

    comments

--- robotvshuman.php
+++ robotvshuman.php
@@ -1,33 +1,41 @@
 
 <?php
 
-// Number Guessing Game
-// Create a way to give feedback to the users, such that:
-// - If they guess correctly, you tell them
-// - If they guess incorrectly, you tell them if it was too high or too low
-// - As bonus, also check to see if the guessed number is out of range
+/* Number Guessing Game (terminal version)
+
+Create a way to give feedback to the users, such that:
+- If they guess correctly, you tell them
+- If they guess incorrectly, you tell them if it was too high or too low
+- As bonus, also check to see if the guessed number is out of range
+*/
 
 // Range
 $min = 1; 
 $max = 50;
 
 // Human choice
-$guess = 50; //Change this value to play!
+$guess = 30; //Change this value to play!
 
 // Robot choice
 $num = rand($min, $max); 
 
 echo "Robot choosed random number. \nHuman picked up " . $guess . ".\n\n";
 
+// Checking if number is out of range
 if($guess < $min ){
     echo "You are too low, try again!";
-} else if ($guess > $max){
+    exit();
+} else if($guess > $max){
     echo "You are too high, try again!";
-} else if($guess === $num){
+    exit();
+} 
+
+// Checking if number is guessed, if not a small hint will be displayed
+if($guess === $num){
     echo "You really got it!";
 } else if($guess > $num){
     echo "You are too high! As a hint, my number was " . ($guess - $num) . (($guess - $num) === 1 ? " number lower than yours :)" : " numbers lower than yours :)");  
-} else if($guess < $num){
+} else {
     echo "You are too low! As a hint, my number was " . ($num - $guess) . (($num - $guess) === 1 ? " number higher than yours :)" : " numbers higher than yours :)");
 }
 
